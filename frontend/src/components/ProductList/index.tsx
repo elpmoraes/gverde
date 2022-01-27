abf084379224cd5a5ec25024139803784fd1da3b
Felipe Moraes <felipe@ibooking.com.br>
2022-01-27 19:28:09 -0300

    Typescript convention

--- frontend/src/components/ProductList/index.tsx
+++ frontend/src/components/ProductList/index.tsx
@@ -1,9 +1,15 @@
 import ProductItem from "components/ProductItem";
 import { useEffect, useState } from "react";
 
-
+interface Product{
+    id: number,
+    name: string,
+    price: number,
+    quantity: number,
+    code: string
+}
 export function ProductList() {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
         fetch('https://61f311e02219930017f50a26.mockapi.io/product')
